3c221d999d4a5cece98b16624e6d51d8bed4c5e0
dkomin415 <dkomin415.sales@gmail.com>
2022-08-06 11:51:43 -0700

    finalized comments models and routes and joined user, post, and users

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
+    order: [["created_at", "DESC"]],
     attributes: [
       "id",
       "post_url",
@@ -16,15 +17,23 @@ router.get("/", (req, res) => {
           "(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)"
         ),
         "vote_count",
-      ],
+      ]
     ],
-    order: [["created_at", "DESC"]],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ["username"],
+        }
+      },
       {
         model: User,
-        attributes: ["username"],
-      },
-    ],
+        attributes: ['username']
+      }
+    ]
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -52,10 +61,18 @@ router.get("/:id", (req, res) => {
     ],
     include: [
       {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ["username"],
+        }
+      },
+      {
         model: User,
         attributes: ["username"],
-      },
-    ],
+      }
+    ]
   })
     .then((dbPostData) => {
       if (!dbPostData) {
@@ -95,7 +112,7 @@ router.post("/", (req, res) => {
         res.status(400).json(err);
       });
   });
-  
+
 router.put("/:id", (req, res) => {
   Post.update(
     {
@@ -124,7 +141,7 @@ router.delete("/:id", (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id,
-    },
+    }
   })
     .then((dbPostData) => {
       if (!dbPostData) {
