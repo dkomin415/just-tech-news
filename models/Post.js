084ce0ff05b666501c0dee66416192c4c5f67174
dkomin415 <dkomin415.sales@gmail.com>
2022-08-06 11:05:39 -0700

    finalized upvote routes

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,35 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Post extends Model {}
+class Post extends Model {
+  static upvote(body, models) {
+    // create the vote
+    return models.Vote.create({
+      user_id: body.user_id,
+      post_id: body.post_id
+    }).then(() => {
+      // then find the post we just voted on
+      return Post.findOne({
+        where: {
+          id: body.post_id,
+        },
+        attributes: [
+          "id",
+          "post_url",
+          "title",
+          "created_at",
+          // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+          [
+            sequelize.literal(
+              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+            ),
+            "vote_count"
+          ]
+        ]
+      });
+    });
+  }
+}
 
 // create fields/columns for Post model
 Post.init(
@@ -10,32 +38,32 @@ Post.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     title: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     post_url: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isURL: true
-      }
+        isURL: true,
+      },
     },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'user',
-        key: 'id'
-      }
-    }
+        model: "user",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post'
+    modelName: "post",
   }
 );
 
