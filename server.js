0a93293fba63b9e1ed9cb3e3e59e517369ac80dc
dkomin415 <dkomin415.sales@gmail.com>
2022-08-06 15:24:43 -0700

    started homepage view using handlebarsjs and updated html routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
+const path = require('path');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +11,10 @@ const PORT = process.env.PORT || 3001;
 // express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
